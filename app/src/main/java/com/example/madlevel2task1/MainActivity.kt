694f7bfce114ebeb496b1284a752a8775037adf0
M-BenAli-HvA <mohamed.ben.ali@hva.nl>
2020-09-03 20:37:12 +0200

    Complete final functionalities and tweak design

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AbsListView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var places: ArrayList<Place> = arrayListOf()
+    private var placeAdapter: PlaceAdapter = PlaceAdapter(places)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(
+            2, RecyclerView.VERTICAL
+        )
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i],
+                Place.PLACE_DRAWABLE_RES_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanged()
+
+
     }
 }
--- app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
+++ app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.madlevel2task1
 
-class PlaceAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.databinding.ItemPlaceBinding
+
+class PlaceAdapter(private var places: ArrayList<Place>) :
+    RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemPlaceBinding.bind(itemView)
+        fun databind(place: Place) {
+            binding.placeIv.setImageResource(place.imageResId)
+            binding.placeTv.text = place.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(places[position])
+    }
 }
